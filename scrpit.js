67f789deb5b4f04491020778f6b41fd55ef7d0f6
Rubens Castelani <rubenscastelani@gmail.com>
2023-08-07 14:30:05 -0300

    add ajust container width

--- scrpit.js
+++ scrpit.js
@@ -8,14 +8,14 @@ function toggleMode() {
   // substituir a imagem
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem gnomesunglasses
-    //img.setAttribute("src", "./assets/gnome_sunglasses.gif")
+    img.setAttribute("src", "./assets/gnome_sunglasses.gif")
     img.setAttribute(
       "alt",
       "Um gnomo respirando usando um chapeu verde, barba longa branca, camisa roxa e um oculos de sol"
     )
   } else {
     // se nao tiver light mode, manter a imagem original
-    //img.setAttribute("src", "./assets/gnome.gif")
+    img.setAttribute("src", "./assets/gnome.gif")
     img.setAttribute(
       "alt",
       "Um gnomo respirando usando um chapeu verde, barba longa branca, camisa roxa e um oculos de grau elevado"
